cf8122ddab2af6efa2e5c8bee5c7882763bf036b
Adam Aljets <aljets@users.noreply.github.com>
2018-02-26 11:04:31 -0800

    add vagrant ssh-type command

--- config.fish
+++ config.fish
@@ -9,6 +9,12 @@ function git_branch
     git rev-parse --abbrev-ref HEAD 2> /dev/null
 end
 
+# Way, way faster Vagrant SSH. Run vagrant ssh-config to find private key location.
+set vagrant_private_key '~/.vagrant.d/boxes/base/0/virtualbox/vagrant_private_key'
+function vs
+    ssh -Y -i $vagrant_private_key -p 2222 vagrant@127.0.0.1
+end
+
 function fish_prompt
     echo -n (prompt_pwd)
     if test -d .hg
