ab9280232186eb326b9ff955996584e8266172e7
Adam Aljets <aljets@users.noreply.github.com>
2021-04-05 12:53:02 -0700

    Add ctrl-d to branch list to delete branches

--- fish/functions/b.fish
+++ fish/functions/b.fish
@@ -1,3 +1,9 @@
 function b
-    git checkout (git branch --all --sort=-committerdate | fzf | tr -d '[:space:][*]')
+    # `b` to open local git branches in fzf sorted by most recent. Could add --all to script to show remote.
+    # enter to open
+    # ctrl-d to delete
+    git branch --sort=-committerdate | fzf \
+        --bind 'ctrl-d:execute(git branch -D (echo {} | tr -d "[:space:][*]"))+reload(git branch --sort=-committerdate)' \
+    # enter to checkout
+    | read -l branchname; and git checkout (echo $branchname | tr -d "[:space:][*]")
 end
